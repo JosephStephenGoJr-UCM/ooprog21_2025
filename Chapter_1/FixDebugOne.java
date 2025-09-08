bd08eecc462062f358d80c12689481597c841f70
JosephStephenGoJr-UCM <gojoseph6969@gmail.com>
2025-09-08 21:48:57 +0800

    Add files via upload

---
+++ Chapter_1/FixDebugOne.java
@@ -0,0 +1,21 @@
+public class FixDebugOne {
+   /* This program displays a greeting */
+   public static void Main(String args)
+   {
+      Systm.out.println("Hello World!")
+   }
+
+/*
+List down the spotted bugs below:
+
+e.g.
+- // wrong syntax for access modifier "publlic" instead of "public"
+
+Bugs:
+1."main" instead of "Main"
+2.No braces after String, it should have been (String[] args)
+3.Syntax typo "System" instead of "Systm"
+4.No semicolon(;) after block of code.
+5.Class names should have no spaces.
+*/   
+}
