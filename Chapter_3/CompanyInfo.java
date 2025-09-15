ce87fd5e17739c584ecf244b4eb1c6a629a607a0
JosephStephenGoJr-UCM <gojoseph6969@gmail.com>
2025-09-15 09:00:15 +0800

    Create CompanyInfo.java

---
+++ Chapter_3/CompanyInfo.java
@@ -0,0 +1,9 @@
+public class CompanyInfo{
+   public static void main(String[] args){
+      System.out.println("Smart Electronics");
+      displayHours();
+   }
+   static void displayHours(){
+      System.out.println("Monday - Friday 8:00 AM to 6:00 PM \nSaturday        8:00 AM to 12:00 NN \nSunday          Closed");
+   }
+}
